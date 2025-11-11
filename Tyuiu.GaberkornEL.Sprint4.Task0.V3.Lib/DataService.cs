7fbe0b8c55caa3e045fa5243133fdb34b8786d5e
Ymir4ik <middem4@gmail.com>
2025-11-11 21:03:35 +0500

    Add project files.

---
+++ Tyuiu.GaberkornEL.Sprint4.Task0.V3.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.GaberkornEL.Sprint4.Task0.V3.Lib
+{
+    public class DataService : ISprint4Task0V3
+    {
+        public int GetMultOddArrEl(int[] array)
+        {
+            int res = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    res *= array[i];
+                }
+            }
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint4.Task0.V3.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GaberkornEL.Sprint4.Task0.V3.Lib;
+namespace Tyuiu.GaberkornEL.Sprint4.Task0.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 8, 9, 4, 1, 2, 3, 4, 5, 6, 7 };
+            int res = ds.GetMultOddArrEl(numsArray);
+            int wait = 945;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
