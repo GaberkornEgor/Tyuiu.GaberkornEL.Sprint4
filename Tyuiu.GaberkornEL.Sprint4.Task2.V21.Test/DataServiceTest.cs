5215b02d9464fbdda3740bb89e17847c024b2d5c
Ymir4ik <middem4@gmail.com>
2025-11-11 21:38:07 +0500

    e

---
+++ Tyuiu.GaberkornEL.Sprint4.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.GaberkornEL.Sprint4.Task2.V21.Lib;
+namespace Tyuiu.GaberkornEL.Sprint4.Task2.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 1, 7, 2, 2, 6, 2, 3, 2, 3, 5, 7, 7, 1 };
+            int res = ds.Calculate(numsArray);
+            int wait = 96;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
