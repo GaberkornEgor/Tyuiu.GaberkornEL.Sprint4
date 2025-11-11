d9e1c21e966e664f9bea4ed41258e6a6e2acfdad
Ymir4ik <middem4@gmail.com>
2025-11-11 22:49:25 +0500

    fgf

---
+++ Tyuiu.GaberkornEL.Sprint4.Task7.V17.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.GaberkornEL.Sprint4.Task7.V17.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint4.Task7.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int n = 3;
+            int m = 3;
+            string value = "753159864";
+
+            int result = ds.Calculate(n, m, value);
+            int wait = 3;
+
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint4.Task7.V17/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GaberkornEL.Sprint4.Task7.V17.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint4.Task7.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:  Обработка структурных типов                                      *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр \"753159864\".                        *");
+            Console.WriteLine("* Преобразуйте ее в матрицу 3 на 3 и подсчитайте количество четных чисел  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int n = 3;
+            int m = 3;
+            string value = "753159864";
+
+            Console.WriteLine("Размер матрицы:" + n + "x" + m);
+            Console.WriteLine("Исходная строка: " + value);
+
+            Console.WriteLine();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            int result = ds.Calculate(n, m, value);
+            Console.WriteLine("Количество четных чисел в матрице: " + result);
+
+            Console.WriteLine();
+            Console.WriteLine("Матрица:");
+            int[,] matrix = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(value[i * m + j].ToString());
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
